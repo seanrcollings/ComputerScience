91d82d0dc98672131a63ae2f78c316ea5b35b48d
Sean Collings <seanrcollings@gmail.com>
2019-11-06 09:22:05 -0700

    assn 13 changes

--- cs1400/Assn13/assn13-task2.py
+++ cs1400/Assn13/assn13-task2.py
@@ -3,7 +3,7 @@ from password import Password
 def main():
     done = False
     password = Password()
-    print("Welcome to the password setter!")    
+    print("Welcome to the password setter!")
     while not done:
         newPassword = input("Please enter a password: ")
 
@@ -15,7 +15,7 @@ def main():
             print(password.getErrorMessage())
 
         cont = input("Do you want to make a new password (y/n) : ")
-        if cont == 'n' or cont == 'no':
+        if cont in ('n', 'no'):
             print("Goodbye!")
             done = True
         else:
